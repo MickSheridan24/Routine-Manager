18120d5126fe67c3f9a04311067f43116d8620cf
Mickey <micksheridan.24@gmail.com>
2019-06-10 15:27:09 -0400

    login logic

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,17 +2,14 @@ const User = require("../models/User");
 const router = require("express").Router();
 
 router.post("/", async (req, res) => {
-  console.log("POST USER ROUTE");
-  const user = await new User(req.body);
-  user.save();
-  console.log("FINALLy", user);
+  const user = await new User(req.body.username);
+  user.save(req.body.password);
   res.send(user);
 });
 
 router.post("/login", async (req, res) => {
-  console.log("logging in", req.body);
-  const token = await User.login(req.body);
-  res.send({ status: token });
+  const status = await User.login(req.body);
+  res.send({ success: status });
 });
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -2,10 +2,8 @@ const client = require("../db/knex");
 const bcrypt = require("bcrypt");
 
 class User {
-  constructor(args) {
-    console.log("USER CONSTRUCTOR", args);
-    this.username = args.username;
-    this.password = args.password;
+  constructor(username) {
+    this.username = username;
   }
 
   static all() {
@@ -17,36 +15,33 @@ class User {
   }
   static async digest(password) {
     const hash = await bcrypt.genSalt(saltRounds, (err, salt) => {
-      console.log(salt);
       bcrypt.hash(password, salt, (err, hash) => {
-        console.log(hash);
         return hash;
       });
     });
-    console.log("made hash", hash);
     return hash;
   }
 
   static async login({ username, password }) {
-    console.log("Model login", username, password);
-    const user = await client
+    const query = await client
       .select()
       .from("users")
-      .where({ username: username })[0];
-    console.log(user);
+      .where("username", username);
+
+    const user = query[0];
     if (user) {
-      const check = await bcrypt.compare(password, user.passwordDigest, (err, res) => {
-        console.log("Status", check);
-        return check;
-      });
+      const match = await bcrypt.compare(password, user.passwordDigest);
+      return match;
+    } else {
+      return false;
     }
   }
 
-  async save() {
+  async save(password) {
     const saltRounds = 8;
     let post = {};
     await bcrypt.genSalt(saltRounds, async (err, salt) => {
-      await bcrypt.hash(this.password, salt, async (err, hash) => {
+      await bcrypt.hash(password, salt, async (err, hash) => {
         post = await client("users").insert({ username: this.username, passwordDigest: hash });
       });
     });
